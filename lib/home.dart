89dea46361cd2111fb450dd84934f7bd2bf3d8b0
juserh <rosapark07@gmail.com>
2021-02-09 16:14:55 +0900

    add fcm

--- lib/home.dart
+++ lib/home.dart
@@ -1,9 +1,8 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:url_launcher/url_launcher.dart';
-
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -11,10 +10,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   Future<void> _launched;
   //사회복지사님한테 연결
-  String _phone='01077777777';
+  String _phone = '01077777777';
 
   Future<void> _makePhoneCall(String url) async {
     if (await canLaunch(url)) {
@@ -41,27 +39,60 @@ class _HomeState extends State<Home> {
     }
   }
 
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  @override
+  void initState() {
+    super.initState();
+
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+      },
+    );
+    _firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(
+            sound: true, badge: true, alert: true, provisional: true));
+    _firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings) {
+      print("Settings registered: $settings");
+    });
+    _firebaseMessaging.getToken().then((String token) {
+      assert(token != null);
+      print('token: $token');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-
     bool checked = false;
 
     return MaterialApp(
-      title: 'connect',
-      home: Scaffold(
-        body: SingleChildScrollView(
+        title: 'connect',
+        home: Scaffold(
+            body: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget> [
+            children: <Widget>[
               const SizedBox(height: 20.0),
               IconButton(
-                icon: Icon(Icons.account_circle_rounded), iconSize: 200, color: Colors.brown[900],
+                icon: Icon(Icons.account_circle_rounded),
+                iconSize: 200,
+                color: Colors.brown[900],
                 onPressed: () {
                   setState(() {});
                 },
               ),
-              Text('눌러서 사용자정보 등록하기', style: TextStyle(fontSize: 25),),
+              Text(
+                '눌러서 사용자정보 등록하기',
+                style: TextStyle(fontSize: 25),
+              ),
               const SizedBox(height: 30.0),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
@@ -208,28 +239,31 @@ class _HomeState extends State<Home> {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                ButtonTheme(
-                  minWidth: 130.0,
-                  height: 80.0,
-                  buttonColor: Colors.brown[300],
-                  child: RaisedButton(
-                    onPressed: ()=> setState((){
-                      _launched = _makePhoneCall('tel:$_phone');
-                    }),
-                    child: Text("사회복지사님\n전화걸기",textAlign: TextAlign.center, style: TextStyle(fontSize: 20)),
-                  ),
-                ),
-                  const SizedBox(width : 30.0),
                   ButtonTheme(
                     minWidth: 130.0,
                     height: 80.0,
                     buttonColor: Colors.brown[300],
                     child: RaisedButton(
-                      onPressed: ()=> setState((){
+                      onPressed: () => setState(() {
+                        _launched = _makePhoneCall('tel:$_phone');
+                      }),
+                      child: Text("사회복지사님\n전화걸기",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 20)),
+                    ),
+                  ),
+                  const SizedBox(width: 30.0),
+                  ButtonTheme(
+                    minWidth: 130.0,
+                    height: 80.0,
+                    buttonColor: Colors.brown[300],
+                    child: RaisedButton(
+                      onPressed: () => setState(() {
                         _launched = _textMe();
                       }),
-
-                      child: Text("문자하기", textAlign: TextAlign.center, style: TextStyle( fontSize: 20)),
+                      child: Text("문자하기",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 20)),
                     ),
                   ),
                   // FutureBuilder<void>(future: _launched, builder: _launchStatus),
@@ -237,8 +271,6 @@ class _HomeState extends State<Home> {
               ),
             ],
           ),
-        )
-      )
-      );
+        )));
   }
 }
