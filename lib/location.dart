e6d81c0a2641d66647b1c470e4c0b2d3759248c7
serim53 <tpfla9953@naver.com>
2021-02-13 23:51:31 +0900

    app icon&report

--- lib/location.dart
+++ lib/location.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Location extends StatefulWidget {
   @override
@@ -6,10 +8,55 @@ class Location extends StatefulWidget {
 }
 
 class LocationState extends State<Location> {
+
+  Future<void> _launched;
+
+  Future<void> _textMe() async {
+    // Android
+    const uri = 'sms: 119?body=<긴급신고>%20발신인: 김OO\n'
+        '위급합니다. 현 위치는';
+    if (await canLaunch(uri)) {
+      await launch(uri);
+    } else {
+      // iOS
+      const uri = 'sms: 119?body=hello%20there';
+      if (await canLaunch(uri)) {
+        await launch(uri);
+      } else {
+        throw 'Could not launch $uri';
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('지도'),
+
+    bool checked = false;
+
+    return MaterialApp(
+        title: 'connect',
+        home: Scaffold(
+            body: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget> [
+                  ButtonTheme(
+                    minWidth: 130.0,
+                    height: 80.0,
+                    buttonColor: Colors.brown[300],
+                    child: RaisedButton(
+                      onPressed: ()=> setState((){
+                        _launched = _textMe();
+                      }),
+
+                      child: Text("위급상황 신고하기", textAlign: TextAlign.center, style: TextStyle( fontSize: 20)),
+                    ),
+                  ),
+                ],
+              ),
+            )
+        )
     );
   }
 }
